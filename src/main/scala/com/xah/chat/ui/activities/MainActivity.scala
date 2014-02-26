b9e20c4906ab438cd8ee01fea1a378b5117277ff
lemonxah <lemon.xah@gmail.com>
2014-02-26 10:30:59 +0200

    ripping out the old ui and the old way of doing things starting fresh

--- src/main/scala/com/xah/chat/ui/activities/MainActivity.scala
+++ src/main/scala/com/xah/chat/ui/activities/MainActivity.scala
@@ -2,7 +2,6 @@ package com.xah.chat.ui.activities
 
 import android.os.Bundle
 import com.xah.chat.R
-import com.xah.chat.ui.fragments.ContactsFragment
 import com.xah.chat.datamodel.xah
 import scala.language.implicitConversions
 import android.content.{Intent, Context}
@@ -15,22 +14,8 @@ class MainActivity extends BaseActivity {
 
   override def onCreate(data: Bundle): Unit = {
     super.onCreate(data)
-    if (TextUtils.isEmpty(MCName)) MCName = xah.MCName(this)
-    val prefs = getSharedPreferences(xah.SHAREDPREFS, Context.MODE_PRIVATE)
-    if (!prefs.contains(xah.PREF_MCNAME) || TextUtils.isEmpty(MCName)) {
-      val intent = new Intent(this, classOf[LoginActivity])
-      startActivity(intent)
-      this.finish()
-    } else {
-      startService(new Intent(this, classOf[XService]))
-    }
-
+    startService(new Intent(this, classOf[XService]))
     setContentView(R.layout.activity_main)
-    if (getFragmentManager.findFragmentById(R.id.content_frame) == null) {
-      getSupportFragmentManager.beginTransaction
-        .replace(R.id.content_frame, new ContactsFragment)
-        .commit
-    }
   }
 }
 
